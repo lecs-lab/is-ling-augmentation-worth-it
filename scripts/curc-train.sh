94bfb51716ba0639a06e581222353fe3ea0dd89e
michaelpginn <milespergallon271@gmail.com>
2024-09-14 12:03:34 -0500

    Fix arg

--- scripts/curc-train.sh
+++ scripts/curc-train.sh
@@ -37,8 +37,8 @@ if [ "$MODE" == "igt" ]; then
     torchrun --nproc_per_node=1 igt_experiments.py train --model_type baseline --aug_mode mixed "$@"
 elif [ "$MODE" == "mt" ]; then
     python mt_experiments.py train --model_type baseline --aug_mode mixed --direction "usp->eng"
-    python mt_experiments.py train --model_type baseline --aug_mode aug_m1 --direction "usp->eng"
-    python mt_experiments.py train --model_type baseline --aug_mode aug_m2 --direction "usp->eng"
+    python mt_experiments.py train --model_type aug_m1 --aug_mode mixed --direction "usp->eng"
+    python mt_experiments.py train --model_type aug_m2 --aug_mode mixed --direction "usp->eng"
     # torchrun --nproc_per_node=1 mt_experiments.py train --model_type baseline --aug_mode mixed "$@"
 elif [ "$MODE" == "segment" ]; then
     echo "Error: segmentation not yet implemented"
