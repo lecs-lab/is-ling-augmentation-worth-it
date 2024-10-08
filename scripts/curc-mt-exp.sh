4a26e059e6da093f04610539cdbce6c8c421ce3e
michaelpginn <milespergallon271@gmail.com>
2024-10-08 17:12:53 -0600

    Add install

--- scripts/curc-mt-exp.sh
+++ scripts/curc-mt-exp.sh
@@ -26,6 +26,8 @@ module load gcc/11.2.0
 source /curc/sw/anaconda3/latest
 conda activate AutoIGT
 
+pip install --upgrade glossing
+
 export STANZA_RESOURCES_DIR="/scratch/alpine/migi8081/stanza/"
 
 cd "/projects/migi8081/morpheme-hallucination/src"
