cb8f1268b5468746001d96c01ca503a8892cc893
michaelpginn <milespergallon271@gmail.com>
2024-11-16 13:32:50 -0700

    Switch to mamba

--- scripts/curc-esp-usp.sh
+++ scripts/curc-esp-usp.sh
@@ -14,8 +14,9 @@
 
 module purge
 module load gcc/11.2.0
-source /curc/sw/anaconda3/latest
-conda activate AutoIGT
+module load mambaforge
+mamba activate igt
+
 export STANZA_RESOURCES_DIR="/scratch/alpine/migi8081/stanza/"
 cd "/projects/migi8081/augmorph/"
 
