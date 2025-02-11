3d1510ef6ab40b7498a778a9b086dcdf908a85e3
michaelpginn <milespergallon271@gmail.com>
2025-02-11 14:05:04 -0700

    Clean up the script and switch to pdf

--- scripts/data-viz/accuracy-individual.py
+++ scripts/data-viz/accuracy-individual.py
@@ -1,4 +1,6 @@
 # %%
+import typing
+
 import pandas as pd
 import seaborn as sns
 from viz_utils import create_filtered_dataframe, method_names
@@ -8,84 +10,77 @@ from viz_utils import create_filtered_dataframe, method_names
 csv_file = input("Enter CSV file name, including its relative path: ")
 
 filtered_df = create_filtered_dataframe.create_filtered_dataframe(csv_file)
-if 'usp' in csv_file:
-    language = 'Uspanteko'
-elif 'arp' in csv_file:
-    language = 'Arapaho'
+if "usp" in csv_file:
+    language = "Uspanteko"
+elif "arp" in csv_file:
+    language = "Arapaho"
+else:
+    raise ValueError()
 
 # %%
 final_df = method_names.method_names(filtered_df)
 
 # %%
 # Isolate baseline and individual method runs
-aug_baseline = (filtered_df['Method'] == 'Baseline') 
-baseline_df = filtered_df[aug_baseline]
+strategies = ["Baseline", "Insert noise"]
 
-aug_noise = (filtered_df['Method'] == 'Insert noise')
-noise_df = filtered_df[aug_noise]
+if language == "Uspanteko":
+    strategies += [
+        "Delete with exclusions",
+        "Random delete",
+        "Insert conjunction",
+        "TAM update",
+        "Random duplicate",
+    ]
+elif language == "Arapaho":
+    strategies += ["Insert noise", "Insert interjection", "Sentence permutations"]
 
-if language == 'Uspanteko':
-    aug_delete_excl = (filtered_df['Method'] == 'Delete with exclusions')
-    delete_excl_df = filtered_df[aug_delete_excl]
-
-    aug_delete = (filtered_df['Method'] == 'Random delete') 
-    delete_df = filtered_df[aug_delete]
-
-    aug_conj = (filtered_df['Method'] == 'Insert conjunction') 
-    conj_df = filtered_df[aug_conj]
-
-    aug_tam = (filtered_df['Method'] == 'TAM update')
-    tam_df = filtered_df[aug_tam]
-
-    aug_dup = (filtered_df['Method'] == 'Random duplicate') 
-    dup_df = filtered_df[aug_dup]
-    
-    result = pd.concat([baseline_df, noise_df, delete_excl_df, delete_df, conj_df, tam_df, dup_df])
-
-
-elif language == 'Arapaho':
-    aug_interjection = (filtered_df['Method'] == 'Insert interjection') 
-    interjection_df = filtered_df[aug_interjection]
-
-    aug_permutations = (filtered_df['Method'] == 'Sentence permutations') 
-    permutation_df = filtered_df[aug_permutations]
-
-    result = pd.concat([baseline_df, noise_df, interjection_df, permutation_df])
-
+df = filtered_df[filtered_df["Method"].isin(strategies)]
+df = typing.cast(pd.DataFrame, df)
 
 # %%
 # BLEU Score visualization
 individual_bleu = sns.relplot(
-    data=result,
-    x="training_size", y="test/BLEU", kind='line', hue='Method', errorbar=None
+    data=df,
+    x="training_size",
+    y="test/BLEU",
+    kind="line",
+    hue="Method",
+    errorbar=None,
 )
-individual_bleu.set_axis_labels('Training Size', 'BLEU Score')
+individual_bleu.set_axis_labels("Training Size", "BLEU Score")
 
 # Output to file
-individual_bleu.savefig('individual_bleu.png')
+individual_bleu.savefig("individual_bleu.pdf", format="pdf")
 
 # %%
 # chrF Score visualization
 individual_chrf = sns.relplot(
-    data=result,
-    x="training_size", y="test/chrF", kind='line', hue='Method', errorbar=None
+    data=df,
+    x="training_size",
+    y="test/chrF",
+    kind="line",
+    hue="Method",
+    errorbar=None,
 )
-individual_chrf.set_axis_labels('Training Size', 'chrF Score')
+individual_chrf.set_axis_labels("Training Size", "chrF Score")
 
 # Output to file
-individual_chrf.savefig('individual_chrf.png')
+individual_chrf.savefig("individual_chrf.pdf", format="pdf")
 
 # %%
 # Loss Curve Visualization
 individual_loss = sns.relplot(
-    data=result,
-    x="training_size", y="test/loss", kind='line', hue='Method', errorbar=None
+    data=df,
+    x="training_size",
+    y="test/loss",
+    kind="line",
+    hue="Method",
+    errorbar=None,
 )
-individual_loss.set_axis_labels('Training Size', 'Loss')
+individual_loss.set_axis_labels("Training Size", "Loss")
 
 # Output to file
-individual_loss.savefig('individual_loss.png')
+individual_loss.savefig("individual_loss.pdf", format="pdf")
 
 print("Done. Check data-viz folder for the outputted PNG files.")
-
-
