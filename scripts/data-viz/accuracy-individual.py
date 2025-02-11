c43f098c047aa0c997e81353b2c82a65040f6961
michaelpginn <milespergallon271@gmail.com>
2025-02-11 14:59:22 -0700

    Add legend script and use relative difference

--- scripts/data-viz/accuracy-individual.py
+++ scripts/data-viz/accuracy-individual.py
@@ -32,7 +32,7 @@ final_df = method_names.method_names(filtered_df)
 
 # %%
 # Isolate baseline and individual method runs
-strategies = ["Baseline", "Insert noise"]
+strategies = ["Insert noise"]
 
 if language == "Uspanteko":
     strategies += [
@@ -45,23 +45,61 @@ if language == "Uspanteko":
 elif language == "Arapaho":
     strategies += ["Insert noise", "Insert interjection", "Sentence permutations"]
 
+baseline_df = filtered_df[filtered_df["Method"] == "Baseline"][
+    ["training_size", "test/BLEU"]
+].copy()
+baseline_df = baseline_df.rename(columns={"test/BLEU": "baseline_BLEU"})
+
 df = filtered_df[filtered_df["Method"].isin(strategies)]
 df = typing.cast(pd.DataFrame, df)
+
+df = pd.merge(df, baseline_df, on="training_size", how="left")
+df["BLEU_diff"] = df["test/BLEU"] - df["baseline_BLEU"]
+
+
+method_colors = {
+    "Baseline": "#aaaaaa",  # grey
+    "Insert noise": "#254653",  # blue
+    "Delete with exclusions": "#299D8F",  # teal
+    "Random delete": "#F4A261",  # light orange
+    "Insert conjunction": "#43E0D8",  # light blue
+    "TAM update": "#E76F51",  # dark orange
+    "Random duplicate": "#E9C46A",  # yellow
+    "Insert interjection": "#43E0D8",  # light blue
+    "Sentence permutations": "#E76F51",  # dark orange
+}
+
+unique_training_sizes = sorted(df["training_size"].unique())
+
+
+def add_grid_lines(facetgrid):
+    for ax in facetgrid.axes.flat:
+        for tx in unique_training_sizes:
+            ax.axvline(
+                tx, color="gray", linestyle="--", alpha=0.2, zorder=0, linewidth=0.5
+            )
+        ax.axhline(0, color="black", linestyle="-", alpha=1.0, zorder=0, linewidth=2)
+
 
 # %%
 # BLEU Score visualization
 individual_bleu = sns.relplot(
     data=df,
     x="training_size",
-    y="test/BLEU",
+    y="BLEU_diff",
     kind="line",
     hue="Method",
+    palette=method_colors,
     errorbar=None,
+    legend=False,
 )
-individual_bleu.set_axis_labels("Training Size", "BLEU Score")
+individual_bleu.set_axis_labels("Training Size", "Δ BLEU")
+add_grid_lines(individual_bleu)
 
 # Output to file
-individual_bleu.savefig(f"{experiment_name}_individual_bleu.pdf", format="pdf")
+individual_bleu.savefig(
+    f"{language}_{experiment_name}_individual_bleu.pdf", format="pdf"
+)
 
 # %%
 # chrF Score visualization
@@ -71,28 +109,31 @@ individual_chrf = sns.relplot(
     y="test/chrF",
     kind="line",
     hue="Method",
+    palette=method_colors,
     errorbar=None,
 )
 individual_chrf.set_axis_labels("Training Size", "chrF Score")
 
 # Output to file
-individual_chrf.savefig(f"{experiment_name}_individual_chrf.pdf", format="pdf")
-
-
-# %%
-# Loss Curve Visualization
-individual_loss = sns.relplot(
-    data=df,
-    x="training_size",
-    y="test/loss",
-    kind="line",
-    hue="Method",
-    errorbar=None,
+individual_chrf.savefig(
+    f"{language}_{experiment_name}_individual_chrf.pdf", format="pdf"
 )
-individual_loss.set_axis_labels("Training Size", "Loss")
 
-# Output to file
-individual_loss.savefig(f"{experiment_name}_individual_loss.pdf", format="pdf")
+
+# # %%
+# # Loss Curve Visualization
+# individual_loss = sns.relplot(
+#     data=df,
+#     x="training_size",
+#     y="test/loss",
+#     kind="line",
+#     hue="Method",
+#     errorbar=None,
+# )
+# individual_loss.set_axis_labels("Training Size", "Loss")
+
+# # Output to file
+# individual_loss.savefig(f"{experiment_name}_individual_loss.pdf", format="pdf")
 
 
 print("Done. Check data-viz folder for the outputted PNG files.")
---
+++ scripts/data-viz/create-legend.py
@@ -0,0 +1,38 @@
+import math
+
+import matplotlib.lines as mlines
+import matplotlib.pyplot as plt
+
+method_colors = {
+    "Insert noise": "#bbbbbb",  # gray
+    "Delete with exclusions": "#299D8F",  # teal
+    "Random delete": "#F4A261",  # light orange
+    "Insert conjunction/interjection": "#43E0D8",  # light blue
+    "TAM update": "#E76F51",  # dark orange
+    "Random duplicate": "#E9C46A",  # yellow
+    "Sentence permutations": "#254653",  # dark blue
+}
+handles = [
+    mlines.Line2D(
+        [], [], color=color, marker="o", linestyle="None", markersize=8, label=label
+    )
+    for label, color in method_colors.items()
+]
+num_columns = math.ceil(len(handles) / 2)
+fig_legend, ax_legend = plt.subplots(figsize=(8, 0.5))
+legend = ax_legend.legend(
+    handles=handles, title="Method", loc="center", ncol=num_columns, frameon=False
+)
+ax_legend.axis("off")
+legend.set_title(None)
+
+fig_legend.canvas.draw()
+bbox = legend.get_window_extent().transformed(fig_legend.dpi_scale_trans.inverted())
+
+fig_legend.savefig(
+    "legend.pdf",
+    format="pdf",
+    bbox_inches=bbox,
+    pad_inches=0,
+)
+plt.close(fig_legend)
